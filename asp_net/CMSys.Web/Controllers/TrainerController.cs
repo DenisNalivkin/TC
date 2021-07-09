0feabfb49f5e71943c2be560037da58a1635db83
Denis Nalivkin <DenisNalivkin@coherentsolutions.com>
2021-07-09 22:19:31 +0300

    I have started doing the fourth task.

--- asp_net/CMSys.Web/Controllers/TrainerController.cs
+++ asp_net/CMSys.Web/Controllers/TrainerController.cs
@@ -1,6 +1,8 @@
-﻿using CMSys.Core.Entities.Catalog;
+﻿using CMSys.Core.Entities;
+using CMSys.Core.Entities.Catalog;
 using CMSys.Core.Repositories;
 using CMSys.Infrastructure;
+using CMSys.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -25,5 +27,54 @@ namespace CMSys.Web.Controllers
             return View(listAllTrainers);
         }
 
+        public IActionResult ShowTrainerList()
+        {
+            IEnumerable<Trainer> listAllTrainers = _uow.TrainerRepository.All();
+            return View(listAllTrainers);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTrainer()
+        {
+            return View(new CreateTrainerModel());
+        }
+
+        [HttpPost]
+        public IActionResult CreateTrainer (CreateTrainerModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UnitOfWorkOptions unitOfWorkOptions = new UnitOfWorkOptions();
+                UnitOfWork allRepositories = new UnitOfWork(unitOfWorkOptions);
+
+                Trainer newTrainer = new Trainer();
+                TrainerGroup trainerGroup = (TrainerGroup)getEntityTrainerGroup(allRepositories, model);
+
+                newTrainer.Description = model.Description;
+                newTrainer.Id = allRepositories.UserRepository.All().Where(user => user.FullName == model.User).Select(user => user.Id).First();
+                newTrainer.TrainerGroup = trainerGroup;
+                newTrainer.TrainerGroupId = trainerGroup.Id;
+                newTrainer.User = allRepositories.UserRepository.All().Where(user => user.FullName == model.User).Select(user => user).First();
+                newTrainer.VisualOrder = model.Order;
+
+                return View("CreateTrainer");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+     
+        private static VisibleEntity getEntityTrainerGroup(UnitOfWork allRepositories, CreateTrainerModel model)
+        {         
+            foreach (var item in allRepositories.TrainerGroupRepository.All())
+            {
+                if (item.Name == model.TrainerGroup)
+                {
+                    return item;
+                }
+            }         
+            return null;
+        }
     }
 }
---
+++ asp_net/CMSys.Web/Models/CreateTrainerModel.cs
@@ -0,0 +1,76 @@
+﻿using CMSys.Core.Entities.Catalog;
+using CMSys.Core.Repositories;
+using CMSys.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSys.Web.Models
+{
+
+    public class CreateTrainerModel
+    {
+        public string User { get; set; }
+
+        [Required]
+        public string TrainerGroup { get; set; }
+
+        [Required]
+        [RegularExpression("([0-9]*)", ErrorMessage = "Error! Use an integer number!")]
+        public int Order { get; set; }
+
+        [StringLength(4000)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        public SelectList SelectListAllActiveUsers { get; set; }
+        public SelectList SelectLisTrainerGroup { get; set; }
+
+        public CreateTrainerModel()
+        {
+            UnitOfWorkOptions unitOfWorkOptions = new UnitOfWorkOptions();
+            UnitOfWork allRepositories = new UnitOfWork(unitOfWorkOptions);
+            SelectListAllActiveUsers = getArrayActiveNameUsers(allRepositories);
+            SelectLisTrainerGroup = getArrayNameTrainerGroup(allRepositories);
+        }
+  
+        public static SelectList getArrayActiveNameUsers(IUnitOfWork AllRepositories)
+        {
+            Trainer resultFindInTrainerRepository = null;
+            List<string> allActiveUsers = new List<string>();
+            foreach (var user in AllRepositories.UserRepository.All())
+            {
+                if(user.EndDate == null)
+                {                   
+                    resultFindInTrainerRepository = AllRepositories.TrainerRepository.Find(user.Id);
+                    if (resultFindInTrainerRepository == null )
+                    {
+                        allActiveUsers.Add(user.FullName);
+                        resultFindInTrainerRepository = null;
+                    }
+              
+                }
+                                       
+            }
+            string [] arrayAllActiveUsers = allActiveUsers.ToArray();
+            Array.Sort(arrayAllActiveUsers);
+            SelectList selectListItems = new SelectList(arrayAllActiveUsers);
+            return selectListItems;
+        }
+        public static SelectList getArrayNameTrainerGroup(IUnitOfWork AllRepositories)
+        {
+            string[] arrayAllTrainerGroup = new string[AllRepositories.TrainerGroupRepository.All().Count()];
+            int number = -1;
+            foreach (var item in AllRepositories.TrainerGroupRepository.All())
+            {
+                number += 1;
+                arrayAllTrainerGroup[number] = item.Name;
+            }
+            SelectList selectListItems = new SelectList(arrayAllTrainerGroup);
+            return selectListItems;
+        }
+    }
+}
